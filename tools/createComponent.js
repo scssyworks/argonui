b0bc1320cfdef81c06e72f8d89ac6040c0e34cca
Sachin Singh <ssingh259@sapient.com>
2019-06-07 21:47:49 +0530

    Boilerplate in progress

--- tools/createComponent.js
+++ tools/createComponent.js
@@ -1,14 +1,26 @@
+#!/usr/bin/env node
 const inquirer = require('inquirer');
-const config = require('./config').createComponent;
 const fs = require('fs-extra');
-const { hasArgs } = require('./args');
+const argv = require('yargs').argv;
+let userConfig = {};
+try {
+  userConfig = require(`${process.cwd()}/config`).createComponent || {};
+} catch (e) {
+  userConfig = {};
+}
+const config = Object.assign({
+  componentsFolder: 'source/templates/components',
+  atomsFolder: 'source/templates/atoms',
+  moleculesFolder: 'source/templates/molecules',
+  componentTemplate: "create-component-templates/class-template.txt",
+  testTemplate: "create-component-templates/test-template.txt",
+  htlTemplate: "create-component-templates/htl-template.txt"
+}, userConfig);
 
 let targetModule = 'component';
-if (hasArgs('atom')) {
-  targetFolder = 'atoms';
+if (argv.atom) {
   targetModule = 'atom'
-} else if (hasArgs('molecule')) {
-  targetFolder = 'molecules';
+} else if (argv.molecule) {
   targetModule = 'molecule'
 }
 
@@ -23,23 +35,23 @@ const questions = [
 function createComponent(name) {
   // Create folder structure
   let componentPath = `${config.componentsFolder}/${name.toLowerCase()}`;
-  if (hasArgs('atom')) {
+  if (argv.atom) {
     componentPath = `${config.atomsFolder}/${name.toLowerCase()}`;
-  } else if (hasArgs('molecule')) {
+  } else if (argv.molecule) {
     componentPath = `${config.moleculesFolder}/${name.toLowerCase()}`;
   }
   fs.mkdirsSync(componentPath);
   // Create files
   fs.writeFileSync(`${componentPath}/_${name}.scss`, '');
   const templateFileName = name.toLowerCase();
-  fs.writeFileSync(`${componentPath}/${templateFileName}-template.html`, `<sly data-sly-template.${templateFileName}_template="$\{@ data, flag\}"></sly>`);
-  if (!(hasArgs('atom') || hasArgs('molecule'))) {
+  fs.writeFileSync(`${componentPath}/${templateFileName}.html`, `<sly data-sly-template.${templateFileName}_template="$\{@ data, flag\}"></sly>`);
+  if (!(argv.atom || argv.molecule)) {
     const jsFileName = `${name.charAt(0).toUpperCase()}${name.substring(1)}`;
     const instanceName = `${name.charAt(0).toLowerCase()}${name.substring(1)}`;
     fs.writeFileSync(`${componentPath}/${jsFileName}.js`, fs.readFileSync(config.componentTemplate, 'utf8').replace(/#component#/g, jsFileName));
     fs.writeFileSync(`${componentPath}/${jsFileName}.spec.js`, fs.readFileSync(config.testTemplate, 'utf8').replace(/#component#/g, jsFileName).replace(/#instance#/g, instanceName));
     fs.writeFileSync(`${componentPath}/ux-model.json`, '{}');
-    const previewHtml = fs.readFileSync(config.pageTemplate, 'utf8').replace(/#name#/g, templateFileName);
+    const previewHtml = fs.readFileSync(config.htlTemplate, 'utf8').replace(/#name#/g, templateFileName);
     fs.writeFileSync(`${componentPath}/ux-preview.hbs`, previewHtml);
   }
   console.log('\x1b[32m%s\x1b[0m', `${targetModule} ${name} has been created!`);
@@ -55,10 +67,10 @@ inquirer.prompt(questions)
       try {
         let folder = config.componentsFolder;
         let moduleName = 'Component';
-        if (hasArgs('atom')) {
+        if (argv.atom) {
           folder = config.atomsFolder;
           moduleName = 'Atom';
-        } else if (hasArgs('molecule')) {
+        } else if (argv.molecule) {
           folder = config.moleculesFolder;
           moduleName = 'Molecule';
         }
@@ -72,13 +84,12 @@ inquirer.prompt(questions)
           createComponent(componentName);
         }
       } catch (e) {
-        console.log(e);
-        console.log('\x1b[31m%s\x1b[0m', 'Something went wrong!');
+        console.log('\x1b[31m%s\x1b[0m', 'Could not create component due to missing or invalid configuration!');
       }
     } else {
-      console.log('\x1b[31m%s\x1b[0m', 'You need to provide a valid component name');
+      console.log('\x1b[31m%s\x1b[0m', 'Please provide a valid component name');
     }
   })
   .catch(() => {
-    console.log('\x1b[31m%s\x1b[0m', 'Something went wrong!');
+    console.log('\x1b[31m%s\x1b[0m', 'Could not create component due to missing or invalid configuration!');
   });
